8863117170cc69c7da42abcb899544bf2927b3e9
tinoschroeter <tino.schroeter@gmail.com>
2022-01-13 17:52:10 +0100

    extract dates

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const commitsPromise = gitToJs("./");
 commitsPromise.then((commits) => {
   //console.log(JSON.stringify(commits, null, 2))
 
-  const questions = commits.filter(item => item.message.match("Time"))
-  console.log(questions)
+  const questions = commits
+    .filter((item) => item.message.match("Time"))
+    .map((item) => item.date.split(" ").slice(1, 4).join("/"));
+
+  console.log(questions);
 });
