44ec60c782c47a9169d16f0517a31bbb98b9e415
Tino Schröter <tino.schroeter@gmail.com>
2022-05-08 21:35:30 +0200

    Time: 188 ms (23.37%), Space: 50.7 MB (66.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 const containsDuplicate = (nums) => {
-    const obj = {};
+    const sorting = nums.sort((a, b) => a - b);
     
-    for(let i = 0; i<nums.length; i++) {
-        const value = nums[i]
-        if(!obj[value]) {
-            obj[value] = true;
-        } else {
-            return true   
-        }
+    
+    for(let i = 0; i<nums.length - 1; i++) {
+        if(nums[i] === nums[i+1]) return true
     }
+    
     return false
 };
