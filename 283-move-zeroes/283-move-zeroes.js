ba0f26298e651c4c24782accfb81bec14efd766e
Tino Schröter <tino.schroeter@gmail.com>
2022-05-09 22:39:01 +0200

    Time: 67 ms (99.96%), Space: 47.2 MB (14.46%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,12 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const moveZeroes = (nums) => {
-
     let pointer = 0;
     
     for(let i = 0; i<nums.length; i++) {
         if(nums[i] !== 0) {
-            nums[pointer++] = nums[i];
+            nums[pointer] = nums[i];
+            pointer++
         }
     }
     
