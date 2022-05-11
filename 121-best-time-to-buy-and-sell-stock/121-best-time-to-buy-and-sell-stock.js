fb9b0ad4fbb6a8f7e2312ab912fac03eb05dc5bd
Tino Schröter <tino.schroeter@gmail.com>
2022-05-11 21:12:03 +0200

    Time: 89 ms (80.76%), Space: 51.8 MB (48.21%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,22 +2,22 @@
  * @param {number[]} prices
  * @return {number}
  */
-    var maxProfit = function(prices) {    
-        debugger
-        let max = 0
-        let p1 = 0
-        let p2 = 1
-
-        while(p2 < prices.length) {    
-            const tempMax = prices[p2] - prices[p1]        
-            if(tempMax > max) max = tempMax
-            if(prices[p1] <= prices[p2]) {
-                p2++
-            } else {
-                p1++
-            }
-            if(p1 === p2) p2++
+const maxProfit = (prices) => {
+    let buy = 0;
+    let sell = 1;   
+    let max = 0;
+    
+    while(buy < prices.length) {
+        
+        if(prices[sell] > prices[buy]) {
+            const current = prices[sell] - prices[buy] ;
+            max = Math.max(current, max);
+        } else {
+            buy = sell;
         }
-
-        return max
-    }; 
+        
+        sell++
+    }
+    
+    return max
+};
