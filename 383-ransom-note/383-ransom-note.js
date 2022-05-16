ac1f158e459d24d511b8ec2922b26328cabcc657
Tino Schröter <tino.schroeter@gmail.com>
2022-05-16 22:58:16 +0200

    Time: 163 ms (17.07%), Space: 44.4 MB (80.08%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+const canConstruct = (ransomNote, magazine) => {
+    
+    
+    const counter = {};
+    
+    for(let val of magazine) {
+        counter[val] ? counter[val] += 1 : counter[val] = 1;
+    }
+    
+    for(let val of ransomNote) {
+        
+        if(counter[val]) {
+            counter[val] -= 1
+        } else {
+            return false
+        }
+    }
+    
+    return true;
+};
