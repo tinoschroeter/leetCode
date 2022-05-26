bba8d69d4e25a990cf44fb04aa34ea9fc5a136b8
Tino Schröter <tino.schroeter@gmail.com>
2022-05-26 22:24:00 +0200

    Time: 84 ms (51.34%), Space: 44.5 MB (33.38%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = (head) => {
+    
+    let curr = head
+    let prev = null
+    
+    while(curr) {
+        
+        const nxt = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    
+    return prev;
+};
