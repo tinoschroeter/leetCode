c9d6b1eac29be430677b719f8405ad25aa2c6bfa
Tino Schröter <tino.schroeter@gmail.com>
2022-11-26 14:48:54 +0100

    Time: 112 ms (39.07%), Space: 42.3 MB (67.74%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    
+    
+    let result = 0;
+    
+    while(n) {
+        n &= (n - 1)
+        result++
+    }
+    
+    return result;
+};
