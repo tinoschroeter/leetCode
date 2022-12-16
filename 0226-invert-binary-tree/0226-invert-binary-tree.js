cfb529c07f5e1c293530cb1585277ad3970d83a4
Tino Schröter <tino.schroeter@gmail.com>
2022-12-16 17:32:03 +0100

    Time: 49 ms (99.88%), Space: 42.7 MB (18.31%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -10,21 +10,22 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-const invertTree = (root) => {
+var invertTree = function(root) {
     
     
-    const dfs = node => {
-        if (node === null) return
-    
+    const dfs = (node) => {
+        
+        if(!node) return 
+        
         const tmp = node.left;
-        node.left = node.right;
+        node.left = node.right
         node.right = tmp;
         
         dfs(node.left);
         dfs(node.right);
     }
     
-    
     dfs(root);
-    return root;
+    
+    return root
 };
