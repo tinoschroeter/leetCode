f7c8934b556478d055cedc3166ea47cd3fda9562
Tino Schröter <tino.schroeter@gmail.com>
2022-12-17 14:56:28 +0100

    Time: 79 ms (88.28%), Space: 45.4 MB (20.78%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,14 +4,17 @@
  */
 var missingNumber = function(nums) {
     
+    const numbers = {};
     
-    const map = {};
-    
-    nums.forEach(item => {
-        if(!map[item]) map[item] = true;
-    })
-    
-    for(let i = 0; i<nums.length + 1; i++) {
-        if(!map[i]) return i;
+    for(let i = 0; i<nums.length; i++) {
+        const n = nums[i];
+        if(!numbers[n]) numbers[n] = true;
     }
+    
+    for(let i = 1; i<nums.length + 1; i++) {
+        
+        if(!numbers[i]) return i
+    }
+    
+    return 0;
 };
