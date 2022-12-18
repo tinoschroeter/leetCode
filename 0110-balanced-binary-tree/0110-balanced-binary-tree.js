72354c832ff9a35ad464924873856dfbf7f404a8
Tino Schröter <tino.schroeter@gmail.com>
2022-12-18 13:27:45 +0100

    Time: 88 ms (79.19%), Space: 48.8 MB (13.29%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    
+    
+    const dfs = (node) => {
+        
+        if(!node) return [true, 0];
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        
+        const balanced = (left[0] && right[0] && Math.abs(left[1] -right[1]) <= 1);
+        
+        return [balanced, 1 + Math.max(left[1], right[1])]
+    }
+    
+    return dfs(root)[0];
+};
