a809dd3788e5678a75c9b945b43e48b65d862594
Tino Schroeter <tino.schroter@eventim.de>
2022-12-23 14:40:35 +0100

    add: solveAgain.sh

---
+++ solveAgain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+last=$(find . -path ./node_modules -prune -o -iname '*.js' -not -name 'index.js' -printf '%T+ %p\n' \
+  | sort \
+  | head -n1)
+
+echo $last
+echo -n ">> "
+cat "$(echo $last|awk '{ print $2 }' FS='/')/README.md" \
+  | grep 'href'|awk '{ print $2}' FS="\""
