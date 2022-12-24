dfe9314c6e5aea1fdb747c5248ea7ba119d086b4
Tino Schröter <tino.schroeter@gmail.com>
2022-12-24 23:50:47 +0100

    Time: 68 ms (95.83%), Space: 43.6 MB (17.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,17 @@
  */
 var twoSum = function(nums, target) {
     
-    const counter = new Map();
-
-    for(let i =0; i<nums.length; i++) {
-
+    const map = new Map();
+    
+    for(let i = 0;i<nums.length; i++) {
+        
         const diff = target - nums[i];
-        if(counter.has(diff)) return [i, counter.get(diff)];
-        counter.set(nums[i], i);
+        
+        if(map.has(diff)) {   
+            return [i, map.get(diff)]
+        }
+        map.set(nums[i], i)
     }
-
+    
     return [];
 };
