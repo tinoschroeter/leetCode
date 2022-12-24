8967475b32ca2b92d73097642d7dc66e150e052c
Tino Schröter <tino.schroeter@gmail.com>
2022-12-24 11:40:08 +0100

    Time: 4553 ms (20.08%), Space: 44.9 MB (67.45%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -4,34 +4,28 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-   
-
-    const bs = arr => {
+     
+    const bs = (arr) => {
+        
         let l = 0;
         let r = arr.length -1;
         
-      while(l<=r) {
-          const mid = Math.round((l + r) / 2);
-          const val = arr[mid];
-          
-          if(val === target) {
-              return true;
-          }
-          
-          if(target > val) {
-              l = mid + 1;
-          } else {
-              r = mid - 1;
-          }
-      } 
-      
-        return false;
+        while(l<=r) {
+            const mid = Math.round((l + r) / 2)
+            
+            if(arr[mid] === target) return true
+            
+            if(arr[mid] > target) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
     }
     
     
-    for(let i = 0; i<matrix.length; i++) {
-        
-        if(bs(matrix[i])) return true
+    for(let val of matrix) {
+        if(bs(val)) return true
     }
     
     return false;
