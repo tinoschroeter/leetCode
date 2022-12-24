0eab02b66405591aa7d23fb7c5f555979f281769
Tino Schröter <tino.schroeter@gmail.com>
2022-12-24 19:40:13 +0100

    Time: 108 ms (91.21%), Space: 49.8 MB (46.40%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,48 +1,48 @@
 
-
 var MinStack = function() {
-    this.Stack = []; 
-    this.minStack = [];
+    
+    this.stack = [];
+    this.min = [];
 };
 
 /** 
  * @param {number} val
  * @return {void}
- 
  */
-
 MinStack.prototype.push = function(val) {
-    this.Stack.push(val);
+    this.stack.push(val);
     
-    if(this.minStack.length === 0) {
-        this.minStack.push(val);    
-    } else {       
-        const min = Math.min(val, this.getMin());
-        this.minStack.push(min);
+    if(this.min.length === 0) {
+        this.min.push(val)
+    } else {
+        const min = Math.min(this.min[this.min.length -1], val);
+        this.min.push(min)
     }
-    
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-     this.Stack.pop();
-     this.minStack.pop();
+    
+    this.min.pop();
+    return this.stack.pop()
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.Stack[this.Stack.length - 1];
+    
+    return this.stack[this.stack.length - 1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.minStack[this.minStack.length - 1];
+    
+    return this.min[this.min.length - 1];
 };
 
 /** 
