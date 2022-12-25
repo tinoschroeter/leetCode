396df864077b2f28c46dbf7a7cb967c435031374
Tino Schröter <tino.schroeter@gmail.com>
2022-12-25 13:12:13 +0100

    Time: 92 ms (76.07%), Space: 46.3 MB (44.28%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,13 +3,20 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-      
+       
+    // time O(n)
+    // space O(1) ???
+    
+    
     const map = {};
     
-    for(let i = 0; i<nums.length; i++) {
-        const val = nums[i];
+    for(let val of nums) {
         map[val] ? map[val] += 1 : map[val] = 1;
     }
     
-    return Object.keys(map).filter(item => map[item] === 1);
+    for(let val of nums) {
+        if(map[val] === 1) {
+            return val
+        }
+    }
 };
