6bb2855ae5eaa7856c5514ca3328ae8432a0a23b
Tino Schroeter <tino.schroter@eventim.de>
2022-12-28 21:23:40 +0100

    add: color output

--- solveAgain.sh
+++ solveAgain.sh
@@ -6,11 +6,17 @@ else
   count=$1
 fi
 
-last=$(find . -path node_modules -prune -o -iname '0*.js' -not -name 'index.js' -printf '%T+ %p\n' \
-  | sort \
-  | head -n$count|tail -n1)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-echo -e $last
-echo -n ">> "
-cat "$(echo $last | awk '{ print $2 }' FS='/')/README.md" \
-  | grep 'href'|awk '{ print $2 }' FS="\""
+for i in $(seq 1 $count); do
+  last=$(find . -path node_modules -prune -o -iname '0*.js' -not -name 'index.js' -printf '%T+ %p\n' \
+    | sort \
+    | head -n$i|tail -n1)
+
+  echo -e "${RED}${i}${NC}. ${GREEN}$last${NC}"
+  echo -en "${GREEN}>>${NC} "
+  cat "$(echo $last | awk '{ print $2 }' FS='/')/README.md" \
+    | grep 'href'|awk '{ print $2 }' FS="\""|head -n1
+  done
