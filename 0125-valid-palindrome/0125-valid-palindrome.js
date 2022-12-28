629e1d4bef5c4dbde88a908312e2218938249cc1
Tino Schröter <tino.schroeter@gmail.com>
2022-12-28 15:55:34 +0100

    Time: 81 ms (85.20%), Space: 44.5 MB (71.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,21 +4,15 @@
  */
 var isPalindrome = function(s) {
     
+    const str = s.replace(/[^a-z0-9]/ig, '');
+     
+    let left = 0, right = str.length-1;
     
-    const str = s.replace(/[^0-9a-zA-Z]/g, '').split('').map(a => a.toLowerCase()).join('');
-    
-    let left = 0;
-    let right = str.length -1;
-    
-    while(left < right) {
+    while(left<right) {     
+        if(str[left].toLowerCase() !== str[right].toLowerCase()) return false
         
-        if(str[left] === str[right]) {
-            left++
-            right--
-        } else {
-            return false;
-        }
+        left++, right--
     }
     
-    return true
+    return true;
 };
