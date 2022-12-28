1fcd3e76d59b7f62ae4bf0361fb46cc58a82c9ef
Tino Schröter <tino.schroeter@gmail.com>
2022-12-28 15:20:11 +0100

    Time: 115 ms (81.90%), Space: 50.2 MB (27.25%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,6 +1,5 @@
 
 var MinStack = function() {
-    
     this.stack = [];
     this.min = [];
 };
@@ -10,13 +9,13 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
-    this.stack.push(val);
+    this.stack.push(val)
     
     if(this.min.length === 0) {
         this.min.push(val)
     } else {
-        const min = Math.min(this.min[this.min.length -1], val);
-        this.min.push(min)
+        const min = Math.min(val, this.min[this.min.length -1]);
+        this.min.push(min);
     }
 };
 
@@ -26,23 +25,21 @@ MinStack.prototype.push = function(val) {
 MinStack.prototype.pop = function() {
     
     this.min.pop();
-    return this.stack.pop()
+    return this.stack.pop(); 
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    
-    return this.stack[this.stack.length - 1];
+    return this.stack[this.stack.length -1]
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    
-    return this.min[this.min.length - 1];
+    return this.min[this.min.length -1];
 };
 
 /** 
