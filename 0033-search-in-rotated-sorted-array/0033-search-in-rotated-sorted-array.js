f7538bf82197f0963bc380558c50cb1dcee95cad
Tino Schröter <tino.schroeter@gmail.com>
2022-12-30 01:23:36 +0100

    Time: 62 ms (92.06%), Space: 42.5 MB (15.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,30 +3,14 @@
  * @param {number} target
  * @return {number}
  */
-const search = (nums, target) => {
+var search = function(nums, target) {
+       
+    let count = 0;
+
+    for(let val of nums) {
+        if(val === target) return count
+        count++
+    }
     
-        let l = 0; 
-        let r = nums.length - 1
-
-        while (l <= r) {
-            mid = (l + r) // 2
-            if(target == nums[mid]) return mid
-
-        // left sorted portion
-            if(nums[l] <= nums[mid]) {
-              if(target > nums[mid] || target < nums[l]) {
-                 l = mid + 1 
-              } else {
-                 r = mid - 1
-              } 
-        // right sorted portion
-            } else {
-              if(target < nums[mid] || target > nums[r]) {
-                  r = mid - 1
-              } else {
-                 l = mid + 1  
-              }        
-            }
-        }
-        return -1
-};
+    return -1
+}
