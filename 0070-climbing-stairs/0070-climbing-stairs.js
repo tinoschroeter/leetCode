29f23b195216903f43186e2a3ad03151962c6e99
Tino Schröter <tino.schroeter@gmail.com>
2022-12-30 20:06:33 +0100

    Time: 69 ms (73.95%), Space: 41.9 MB (55.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  
-
     
-    const mem = new Map();
-    mem.set(0,0).set(1,1).set(2,2).set(3,3);
-    
-    
-    let idx = 0;
-    let sum = 0;
-    
-    while(idx <= n) {
-        if(!mem.has(idx)) mem.set(idx,  mem.get(idx-1) + mem.get(idx-2));
-        idx++
+    const mem = {
+        1: 1,
+        2: 2,
+        3: 3
     }
     
-    return mem.get(n);
+    const rec = (val) => {
+        if(mem[val]) return mem[val];
+        return mem[val] = rec(val-2) + rec(val-1);
+    }
+    
+    return rec(n)
 };
