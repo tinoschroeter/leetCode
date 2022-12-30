4cfb8faefd324d0a7e84ef4c47e447a091e59750
Tino Schröter <tino.schroeter@gmail.com>
2022-12-30 19:39:29 +0100

    Time: 74 ms (97.34%), Space: 43.8 MB (67.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,22 +4,22 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-
-    if(s.length !== t.length) return false
     
-    const map = {}
-    
-    for(let val of s) {    
-        map[val] ? map[val] += 1 : map[val] = 1
+    const counter = {};
+    let count = 0;
+    for(let val of s) {
+        counter[val] ? counter[val] += 1 : counter[val] = 1;
+        count++
     }
     
     for(let val of t) {
-        if(map[val]) {
-            map[val] -= 1   
+        if(counter[val]) {
+            counter[val] -= 1
+            count--
         } else {
-            return false
+            return false;
         }
     }
     
-    return true
+    return count === 0
 };
