04634e1b5365b29c1e91434de821147ca43e55e9
Tino Schröter <tino.schroeter@gmail.com>
2022-12-30 18:53:24 +0100

    Time: 78 ms (76.21%), Space: 45 MB (35.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const counter = {}
+
+    const counter = {};
     
-    for(let i = 0; i<nums.length; i++) {    
-        if(counter[nums[i]]) {
-            counter[nums[i]] += 1;
-        } else {
-            counter[nums[i]] = 1;
-        }
+    for(let val of nums) {
+        counter[val] ? counter[val] += 1 : counter[val] = 1;
     }
     
-    return Object.keys(counter).sort((a, b) => counter[b] - counter[a])[0];
+    return Object.keys(counter).sort((a,b) => counter[b] - counter[a])[0];
 };
