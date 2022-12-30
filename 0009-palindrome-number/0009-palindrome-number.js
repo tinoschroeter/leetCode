bcca79fa46447a976fbd9319eab1791c16fb03a4
Tino Schröter <tino.schroeter@gmail.com>
2022-12-30 18:27:16 +0100

    Time: 165 ms (94.93%), Space: 50.4 MB (86.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,18 +4,21 @@
  */
 var isPalindrome = function(x) {
     
-    const str = x.toString();
+    if(x < 0) return false; 
     
+    
+    const str = x.toString()
     let l = 0, r = str.length -1;
     
-    
     while(l<r) {
-        if(str[l] !== str[r]) {
+        if(str[l] === str[r]) {
+            l++
+            r--
+        } else {
             return false
         }
-        l++
-        r--
     }
     
-    return true
+    
+    return true;
 };
