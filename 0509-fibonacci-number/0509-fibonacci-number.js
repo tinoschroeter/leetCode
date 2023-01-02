d125987c11b5ea79df3dca33faa274e52d594cd3
Tino Schröter <tino.schroeter@gmail.com>
2023-01-02 22:22:30 +0100

    Time: 178 ms (15.87%), Space: 41.8 MB (54.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -4,9 +4,9 @@
  */
 var fib = function(n) {
     
-    if(n === 0) return 0
-    if(n <= 2) return 1
     
+    if(n === 0) return 0
+    if(n < 2) return 1
     
     return fib(n -2) + fib(n -1);
 };
