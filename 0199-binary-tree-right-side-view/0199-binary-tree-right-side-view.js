6f8ae830de6fcc346960a716f2d84448ae15b3dd
Tino Schröter <tino.schroeter@gmail.com>
2023-01-05 11:35:48 +0100

    Time: 106 ms (28.91%), Space: 44.1 MB (22.28%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    
+    
+    if(!root) return []
+    
+    const queue = [root]
+    const result = []
+    
+    
+    while(queue.length) {
+        
+        result.push(queue[queue.length -1].val)
+        
+        let len = queue.length
+        while(len--) {
+            const node = queue.shift()
+            
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+    }
+    
+    return result;
+};
