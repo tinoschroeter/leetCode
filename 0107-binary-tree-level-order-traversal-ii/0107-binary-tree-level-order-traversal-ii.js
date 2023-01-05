a8ba7b31d521e9c6246821063dadfdaaa3c26bb4
Tino Schröter <tino.schroeter@gmail.com>
2023-01-05 11:41:54 +0100

    Time: 114 ms (20.78%), Space: 44.1 MB (55.42%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    
+    if(!root) return [];
+    
+    
+    const queue = [root]
+    const result = []
+    
+    while(queue.length) {
+        
+        
+        result.push(queue.map(item => item.val));
+        
+        let len = queue.length;
+        
+        while(len--) {
+            const node = queue.shift();
+            
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+    }
+    
+    return result.reverse()
+};
