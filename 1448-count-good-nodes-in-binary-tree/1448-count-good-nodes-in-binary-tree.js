89d921cf51740403ca025d7da1c2ca82ef6762bd
Tino Schröter <tino.schroeter@gmail.com>
2023-01-06 12:03:31 +0100

    Time: 287 ms (5.26%), Space: 70.5 MB (23.59%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    
+    
+    const dfs = (node, maxVal) => {
+        
+        if(!node) return 0
+        
+        let res = node.val >= maxVal ? 1 : 0;
+        maxVal = Math.max(maxVal, node.val);
+        
+        res += dfs(node.left, maxVal);
+        res += dfs(node.right, maxVal);
+        
+        return res
+    }
+    
+    
+    return dfs(root, root.val);
+};
