ea66bf3b5ddacf965ca7db317807d8f5833769a6
Tino Schröter <tino.schroeter@gmail.com>
2023-01-09 17:05:15 +0100

    Time: 68 ms (82.10%), Space: 43.7 MB (17.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,30 +11,25 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-      
-    const arr = [];
+
+    const toArray = [];
     let node = head;
     
     while(node) {
-        arr.push(node.val)
-        node = node.next;
-    }
-    
-    arr.splice(arr.length - n, 1);
-    
-    if(arr.length === 0) return head.next
-    
-    node = head;
-    
-    for(let i = 0; i<arr.length; i++) {
-        node.val = arr[i];
-        if(i === arr.length - 1) {
-            node.next = null
-        }
+        toArray.push(node.val)
         node = node.next
-    }
+    } 
     
-    return head
+    toArray.splice(toArray.length - n, 1);
+    
+    node = new ListNode(77);
+    let res = node
+    
+    toArray.forEach(item => {
+        
+        node.next = new ListNode(item)
+        node = node.next;
+    })
+    
+    return res.next;
 };
-
-
