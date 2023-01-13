caae25afc5f652ed21f96a9c5b9bdb471435b581
Tino Schröter <tino.schroeter@gmail.com>
2023-01-13 11:19:59 +0100

    Time: 64 ms (82.93%), Space: 41.9 MB (62.52%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,24 +3,24 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
+   
     
-    
-    for(let i = digits.length -1; i>=0; i--) {  
+    for(let i = digits.length-1; i>=0; i--) {
         
         const addOne = digits[i] + 1;
         
-        if(addOne > 9) {;
-            digits[i] = 0;
-         } 
-         if(i === 0 && addOne > 9) {
-            digits.unshift(1); 
-         } 
-         if(addOne < 10) {
-            digits[i] = addOne;
-             break;
-         }  
+        if(addOne > 9) {
+            digits[i] = 0
+        }
         
-
+        if(i === 0 && addOne > 9) {
+            digits.unshift(1)
+        }
+        
+        if(addOne < 10) {
+            digits[i] = addOne;
+            break
+        }
     }
     
     return digits;
