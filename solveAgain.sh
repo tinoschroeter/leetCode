6311f49b133822190530fee24a06b7f8ebb462b3
Tino Schroeter <tino.schroter@eventim.de>
2023-01-14 11:15:15 +0100

    update: solveAgain.sh

--- solveAgain.sh
+++ solveAgain.sh
@@ -10,10 +10,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-for i in $(seq 1 $count); do
+for i in $(seq 1 "$count"); do
   last=$(find . -path node_modules -prune -o -iname '0*.js' -not -name 'index.js' -printf '%T+ %p\n' \
     | sort \
-    | head -n$i|tail -n1)
+    | head -n "$i" | tail -n1)
 
   echo -e "${RED}${i}${NC}. ${GREEN}$last${NC}"
   echo -en "${GREEN}>>${NC} "
