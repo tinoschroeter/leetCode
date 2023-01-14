8f7fa0c494e06cc0e25bb7761431577d20659ae8
Tino Schröter <tino.schroeter@gmail.com>
2023-01-14 11:09:03 +0100

    Time: 58 ms (96.98%), Space: 42.9 MB (46.12%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    
+    
+    const stack = [];
+    
+    for(let val of logs) {
+        
+        if(val === '../') {
+            stack.pop()
+        } else if(val === './') {
+            continue
+        } else {
+            stack.push(val);
+        }
+    }
+    
+    return stack.length;
+};
