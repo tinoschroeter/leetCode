67f87f844c4c7173faad1d6fb1ddfb5c40356ffd
Tino Schröter <tino.schroeter@gmail.com>
2023-01-14 01:43:40 +0100

    Time: 65 ms (89.88%), Space: 45 MB (59.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -5,22 +5,20 @@
  */
 var search = function(nums, target) {
     
-      
+    
     let l = 0, r = nums.length -1;
     
     while(l<=r) {
         
-        
         const mid = Math.round((l + r) / 2);
         
-        if(nums[mid] === target) return mid
-        
+        if(nums[mid] === target) return mid;
         if(target < nums[mid]) {
-            r =  mid -1;
+            r = mid -1
         } else {
-            l = mid + 1;
+            l = mid +1
         }
     }
     
-    return -1
-}
+    return -1;
+};
