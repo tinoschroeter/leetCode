0b5b6deab43c38f99f485d4e2e13a314253f6d3f
Tino Schröter <tino.schroeter@gmail.com>
2023-01-15 14:40:15 +0100

    Time: 68 ms (71.10%), Space: 42.2 MB (49.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,25 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    
 
-    let stack = []
-    
-    for(let val of nums) {
-        if(val === 2) stack.push(2)    
-    }
-    
-    for(let val of nums) {
-        if(val === 1) stack.push(1)
-    }
-    
-    for(let val of nums) {
-        if(val === 0) stack.push(0)
-    }
-    
-    
-    for(let i = 0; i<nums.length; i++) {
+    const result = [[],[],[]]
         
-        nums[i] = stack.pop();
-    }
+    nums.forEach(item => {    
+        if(item === 0) result[0].push(0)
+        if(item === 1) result[1].push(1)
+        if(item === 2) result[2].push(2)
+    })
+    
+    result.flat().forEach((item, i) => nums[i] = item);
 };
