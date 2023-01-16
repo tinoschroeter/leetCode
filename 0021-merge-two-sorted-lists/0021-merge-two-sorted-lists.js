6e41bc9e536f3a387aab6d0a0d483fe17d88197f
Tino Schröter <tino.schroeter@gmail.com>
2023-01-16 18:34:35 +0100

    Time: 87 ms (51.29%), Space: 45.2 MB (5.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -12,24 +12,29 @@
  */
 var mergeTwoLists = function(list1, list2) {
     
-    let head = new ListNode(77)
-    let node = head;
     
-    while(list1 && list2) {
-     
-        if(list1.val < list2.val) {
-            head.next = new ListNode(list1.val)
-            list1 = list1.next
-        } else {
-            head.next = new ListNode(list2.val)
-            list2 = list2.next
-        }
-        head = head.next
-        
+    const a1 = [];
+    while(list1) {
+        a1.push(list1.val);
+        list1 = list1.next
     }
     
-    if(list1) head.next = list1
-    if(list2) head.next = list2
+    const a2 = [];
+    while(list2) {
+        a2.push(list2.val);
+        list2 = list2.next
+    }
     
-    return node.next;
+    const s = [...a1, ...a2].sort((a,b) => a-b);
+    
+    let node = new ListNode(77);
+    let head = node;
+    
+    for(let val of s) {
+        node.next = new ListNode(val);
+        node = node.next
+    }
+    
+    
+    return head.next
 };
