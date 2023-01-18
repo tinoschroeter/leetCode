f096b8a334a193dc492eba41992001d3d083ff98
Tino Schröter <tino.schroeter@gmail.com>
2023-01-18 21:01:01 +0100

    Time: 126 ms (51.68%), Space: 49.1 MB (16.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,6 +3,34 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    // O(n log n)
-    return nums.map(a => a*a).sort((a,b)=>a-b);
+    
+    const neg = nums.filter(item => item < 0).map(item => item * item).reverse();
+    const pos = nums.filter(item => item >= 0).map(item => item * item);
+    
+    const result = [];
+    
+    let p1 = 0, p2 = 0;
+    
+    while(neg.length > p1 && pos.length > p2) {
+    
+        if(neg[p1] < pos[p2]) {
+            result.push(neg[p1])
+            p1++
+        } else {
+            result.push(pos[p2])
+            p2++
+        }
+    }
+    
+    while(neg.length > p1) {
+        result.push(neg[p1])
+        p1++
+    }
+    
+    while(pos.length > p2) {
+        result.push(pos[p2])
+        p2++
+    }
+    
+    return result
 };
