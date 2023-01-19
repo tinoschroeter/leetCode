cf9ad6e1990c5e3b1647a790586c16113088c4b7
Tino Schröter <tino.schroeter@gmail.com>
2023-01-19 17:29:14 +0100

    Time: 80 ms (63.45%), Space: 46.8 MB (22.42%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,20 +10,22 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-
-var isValidBST = function(root, min = -Infinity, max = Infinity) {
-    const isBaseCase = root === null;
-    if (isBaseCase) return true;
-
-    const isInvalid = (root.val <= min) || (max <= root.val);
-    if (isInvalid) return false;
-
-    return dfs(root, min, max);
+var isValidBST = function(root) {
+    
+    const result = [];
+    const dfs = node => {
+        if(!node) return 
+        
+        if(node.left) dfs(node.left);
+        result.push(node.val)
+        if(node.right) dfs(node.right);
+    };
+    
+    dfs(root)
+    
+    for(let i = 0; i<result.length -1; i++) {
+        if(result[i] >= result[i+1]) return false
+    }
+    
+    return true
 };
-
-const dfs = (root, min, max) => {
-    const left = isValidBST(root.left, min, root.val);
-    const right = isValidBST(root.right, root.val, max);
-
-    return left && right;
-}
