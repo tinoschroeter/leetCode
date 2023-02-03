b53cdf0ae9139b8e047d9c2c271c5349386fcd02
Tino Schröter <tino.schroeter@gmail.com>
2023-02-03 22:20:52 +0100

    Time: 69 ms (53.39%), Space: 42.3 MB (11.46%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -11,14 +11,13 @@
  */
 var middleNode = function(head) {
     
-    
     let slow = head
-    let fast = head;
+    let fast = head
     
     while(fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
+        slow = slow.next
+        fast = fast.next.next
     }
     
-    return slow
+    return slow;
 };
