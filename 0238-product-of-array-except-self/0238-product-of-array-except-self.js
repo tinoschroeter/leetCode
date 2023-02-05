a876db6eb2d98e43386ef507a36a201c5ca4ba9e
Tino Schröter <tino.schroeter@gmail.com>
2023-02-05 12:34:17 +0100

    Time: 111 ms (69.31%), Space: 58.8 MB (5.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,24 +4,37 @@
  */
 var productExceptSelf = function(nums) {
     
-    
-    
-    // 1 2 3 4
-   //  4 3 2 1
-    
+    const prefix = [];
+    const postfix = [];
     const result = [];
     
-    for(let i = 0; i<nums.length; i++) {
-        
-        let prod = 1;
-        for(let j =0; j<nums.length; j++) {
-            if(i !== j) {
-                prod *= nums[j]
-            }
-        }
-        
-        result.push(prod)
-    }
     
+    nums.forEach((item, i) => {
+        if(i === 0) {
+            prefix[i] = item
+        } else {
+            prefix[i] = item * prefix[i -1];
+        }
+    })
+    prefix.unshift(1);
+    console.log(prefix)
+    
+    nums.reverse().forEach((item, i) => {
+        if(i === 0) {
+            postfix[i] = item;
+        } else {
+            postfix[i] = item * postfix[i-1];
+        }
+    })
+    postfix.unshift(1);
+    postfix.reverse()
+    console.log(postfix)
+    
+    prefix.forEach((item, i) => {
+        result[i] = prefix[i] * postfix[i+1]
+    })
+    
+    result.pop();
     return result;
+    
 };
