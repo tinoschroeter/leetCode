19e56dba74fdd2be023ff4bbdfc7e7a5790b9455
Tino Schröter <tino.schroeter@gmail.com>
2023-02-05 12:09:52 +0100

    Time: 443 ms (42.98%), Space: 71.7 MB (9.91%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    
+  
     const map = {};
     
     for(let val of nums) {
-        map[val] ? map[val] += 1 : map[val] = 1;
-    } 
+        map[val] = true;
+    };
     
     let max = 0;
     
     for(let val of nums) {
         
         if(!map[val -1]) {
-            let seq = val;
-            let counter = 1;
             
-            while(map[seq]) {
-                max = Math.max(max, counter);
-                seq++
-                counter++
+            let tmp = 0;
+            
+            while(map[val++]) {
+                tmp++;
+                max = Math.max(max, tmp);
             }
         }
     }
