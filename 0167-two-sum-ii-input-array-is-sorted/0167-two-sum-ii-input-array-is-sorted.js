cccbca751d8830b34de241cab99b70796f77df08
Tino Schröter <tino.schroeter@gmail.com>
2023-02-15 14:09:09 +0100

    Time: 72 ms (51.95%), Space: 43 MB (68.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -5,17 +5,15 @@
  */
 var twoSum = function(numbers, target) {
     
+    
     let l = 0, r = numbers.length -1;
     
-    while(l<r) {
+    while(l < r) {
         const sum = numbers[l] + numbers[r];
-        
         if(sum === target) return [l+1, r+1];
         
-        if(sum < target) {
-            l++
-        } else {
-            r--
-        }
+        (target > sum) ? l++ : r--;
     }
+    
+    return [];
 };
