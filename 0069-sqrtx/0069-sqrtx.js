89dd40224f3fa586a8c4e80e6e859466033dab9f
Tino Schröter <tino.schroeter@gmail.com>
2023-03-04 22:23:40 +0100

    Time: 99 ms (41.20%), Space: 42.3 MB (92.31%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+        if(x < 2) return x;
+    
+    for(let k=2; k <= x; k++) {
+        if(k * k === x) {
+            return k - k % 1
+        }else if((k * k) > x) {
+            return k - 1 - k % 1
+        }
+    }
+};
