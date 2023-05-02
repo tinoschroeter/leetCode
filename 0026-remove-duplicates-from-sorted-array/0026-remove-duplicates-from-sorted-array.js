0c982fe75cb9094cce46f68bf19475c871070b32
Tino Schröter <tino.schroeter@gmail.com>
2023-05-02 23:14:23 +0400

    Time: 133 ms (19.85%), Space: 44.6 MB (68.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,13 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    
+const removeDuplicates = (nums) => {
     for(let i = 1; i<nums.length; i++) {
-        
-        if(nums[i] === nums[i-1]) {
-            nums.splice(i, 1)
-            i--
-        }
+    if(nums[i] === nums[i-1]) {
+        nums.splice(i, 1)
+        i--
+      }
     }
 };
