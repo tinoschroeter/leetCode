f7dab5f3a60cdf70ce992df27255c300c3f64d66
Tino Schröter <tino.schroeter@gmail.com>
2023-05-08 21:49:05 +0400

    Time: 53 ms (97.85%), Space: 44.1 MB (49.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
-    const map = { max: [0, 0] };
+   
+    const counter = {};
     
     nums.forEach(item => {
-        map[item] ? map[item] += 1 : map[item] = 1;
-        
-        if(map.max[0] < map[item]) {
-            map.max[0] = map[item]
-            map.max[1] = item;
-        }
-    })
+        counter[item] ? counter[item] += 1 : counter[item] = 1
+    });
     
-    return map.max[1]
+    let max = [0, 0];
+    for(let i in counter) {
+        if(counter[i] > max[1]) {
+            max = [i, counter[i]];
+        }
+    }
+    
+    return max[0];
 };
