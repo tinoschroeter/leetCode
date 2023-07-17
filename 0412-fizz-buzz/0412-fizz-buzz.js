a1ce22d825bbb5bbd3f4852a3831b40176b9589a
Tino Schröter <tino.schroeter@gmail.com>
2023-07-17 10:38:36 +0400

    Time: 68 ms (52.93%), Space: 44.2 MB (73.14%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -4,18 +4,15 @@
  */
 var fizzBuzz = function(n) {
     
-    const result = [];
+    const result = []
     
-    for(let i = 1; i<=n; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz")
-        } else if(i % 3 === 0) {
-            result.push("Fizz")
-        } else if(i % 5 === 0) {
-            result.push("Buzz")
-        } else {
-            result.push(i.toString())
-        }
+    for(let i = 1; i<= n; i++) {
+        let str = ""
+    
+        if(i % 3 === 0) str += "Fizz";
+        if(i % 5 === 0) str += "Buzz";
+        
+        result.push(str.length ? str : i.toString())
     }
     
     return result;
