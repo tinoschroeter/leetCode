cca1571077d5e53ac82ec9c80aaa55f49ad71fae
Tino Schröter <tino.schroeter@gmail.com>
2023-07-19 01:01:49 +0400

    Time: 81 ms (42.58%), Space: 42.4 MB (5.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,12 +2,9 @@
  * @param {number} n
  * @return {number}
  */
-const fib = (n) => {
+var fib = function(n) {
+    if(n === 0) return 0;
+    if(n <= 2) return 1;
     
-    // 0 1 1 2 3 5 ...
-    if(n === 0) return 0
-    if(n === 1) return 1
-    if(n === 2) return 1
-    
-    return fib(n -1) + fib(n -2);
+    return fib(n -2) + fib(n -1);
 };
