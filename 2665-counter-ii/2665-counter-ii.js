4a6ff1f655c8462147d99c3de7981d43cd822e63
Tino Schröter <tino.schroeter@gmail.com>
2023-10-21 11:01:45 +0200

    Time: 66 ms (16.00%), Space: 44 MB (95.25%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    
+    
+    let counter = init;
+    
+    const increment = () => {
+        return ++counter;
+    }
+    
+    const decrement = () => {
+        return --counter;
+    }
+    
+    const reset = () => {
+        counter = init;
+        return counter;
+    }
+    
+    return {increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
