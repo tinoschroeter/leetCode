99f1dae3d9cae20ed94a8a55dbf068a3f4e2ffd7
Tino Schröter <tino.schroeter@gmail.com>
2023-10-22 11:47:58 +0200

    Time: 38 ms (99.41%), Space: 42.9 MB (17.34%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -7,13 +7,5 @@
 var reduce = function(nums, fn, init) {
     
     
-    let result = init;
-    
-    for(let i = 0; i<nums.length; i++) {
-        const val = nums[i];
-        
-        result = fn(result, val);
-    }
-    
-    return result;
+    return nums.reduce(fn, init);
 };
