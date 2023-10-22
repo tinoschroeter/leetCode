1396c6f30f01e7ff8f0f630ec3df1aea851c3820
Tino Schröter <tino.schroeter@gmail.com>
2023-10-22 15:12:30 +0200

    Time: 52 ms (47.30%), Space: 42.1 MB (18.94%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -2,7 +2,10 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-const argumentsLength = (...args) => args.length;
+var argumentsLength = function(...args) {
+	
+    return args.length;
+};
 
 /**
  * argumentsLength(1, 2, 3); // 3
