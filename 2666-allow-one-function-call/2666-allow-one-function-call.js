7f0c60ca8ea3c018c55a9716e6b2c2790113c5c9
Tino Schröter <tino.schroeter@gmail.com>
2023-10-22 15:13:48 +0200

    Time: 56 ms (27.79%), Space: 41.4 MB (93.03%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -7,8 +7,8 @@ var once = function(fn) {
     let call = false;
     
 	return function(...args){
-    
-        if(call) return undefined;
+        
+        if(call) return undefined
         
         call = true;
         
