7d9f794413c6a6863f4b1be8f4ce1921c0b2a304
Tino Schröter <tino.schroeter@gmail.com>
2023-10-31 15:43:26 +0100

    Time: 61 ms (65.46%), Space: 41.5 MB (94.99%) - LeetHub

--- 2725-interval-cancellation/2725-interval-cancellation.js
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -6,13 +6,14 @@
  */
 var cancellable = function(fn, args, t) {
     
+    
     fn(...args);
     
     const timer = setInterval(() => fn(...args), t);
     
-    const cancelFn = () => clearInterval(timer);
+    return cancelFn = () => clearInterval(timer);
     
-    return cancelFn;
+    
 };
 
 /**
