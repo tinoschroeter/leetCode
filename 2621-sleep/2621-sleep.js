4705aa15f041f45fed36a215d13cc0557a8ee840
Tino Schröter <tino.schroeter@gmail.com>
2023-11-25 12:03:18 +0100

    Time: 47 ms (84.55%), Space: 41.6 MB (66.74%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,10 +3,8 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-   
     
     return new Promise((resolve, reject) => {
-        
         setTimeout(() => resolve(), millis);
     })
 }
