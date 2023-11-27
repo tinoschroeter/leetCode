800efd0c13d5c52ba0f8fa2ac61a5d8e4e587c15
Tino Schröter <tino.schroeter@gmail.com>
2023-11-27 21:28:53 +0100

    Time: 81 ms (91.58%), Space: 61.9 MB (76.97%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -10,15 +10,16 @@ var flat = function (arr, n) {
     const helper = (arr, count) => {
         
         for(let val of arr) {
+            
             if(Array.isArray(val) && count < n) {
-                helper(val, count +1)
+                helper(val, count + 1)
             } else {
-                res.push(val);
+                res.push(val)
             }
         }
         
         return res
     }
     
-    return helper(arr, 0); 
+    return helper(arr, 0);
 };
