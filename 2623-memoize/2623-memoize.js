12022f8215532ed4a7a2062f0be16d98c9677a52
Tino Schröter <tino.schroeter@gmail.com>
2023-11-29 21:27:35 +0100

    Time: 288 ms (52.86%), Space: 106.8 MB (79.18%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -4,13 +4,14 @@
  */
 function memoize(fn) {
     
+    
     const cache = new Map();
     
     return function(...args) {
-    
+        
         const key = JSON.stringify(args);
         
-        if(cache.has(key)) return cache.get(key);
+        if(cache.has(key)) return cache.get(key)
         
         const result = fn(...args);
         cache.set(key, result);
