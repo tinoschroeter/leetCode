ff42cd57a33663ce194ca94dc99031df874bbeab
Tino Schröter <tino.schroeter@gmail.com>
2023-12-14 13:50:48 +0100

    Time: 57 ms (36.20%), Space: 43.9 MB (11.48%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    
+    let result = init;
+    
+    for(let val of nums) {
+        result = fn(result, val);
+    }
+    
+    
+    return result;
+};
