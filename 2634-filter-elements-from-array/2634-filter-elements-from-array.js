59328d2490fad47e63127ccab25a6f21c89a2dde
Tino Schröter <tino.schroeter@gmail.com>
2023-12-17 18:44:17 +0100

    Time: 51 ms (60.05%), Space: 42.1 MB (22.80%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,13 +5,5 @@
  */
 var filter = function(arr, fn) {
     
-    const result = []
-    
-    for(let i = 0; i<arr.length; i++) {
-        
-        const val = arr[i]
-        if(fn(val, i)) result.push(val)
-    }
-    
-    return result;
+    return arr.filter(fn);
 };
