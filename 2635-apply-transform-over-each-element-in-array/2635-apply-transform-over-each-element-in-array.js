08be45a08bbf972a80786029ff36cc4da0200483
Tino Schröter <tino.schroeter@gmail.com>
2023-12-17 18:43:34 +0100

    Time: 46 ms (84.16%), Space: 42.4 MB (6.54%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -5,13 +5,6 @@
  */
 var map = function(arr, fn) {
     
-    const result = [];
     
-    for(let i = 0; i<arr.length; i++) {
-    
-        const val = arr[i];
-        result.push(fn(val, i))
-    }
-    
-    return result;
+    return arr.map(fn);
 };
